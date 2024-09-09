bc400c6816bb1338b2e8a5fa431b3f358acf18ea
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-09 12:51:50 +0300

    add slug

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from pytils.translit import slugify
 
 from blog.models import Blog
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -7,15 +8,37 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
 class BlogCreateView(CreateView):
     model = Blog
     fields = ("title", "post", "image",)
-    success_url = reverse_lazy('blog:blog_list'),
+    success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ("title", "post", "image")
     success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
 
 class BlogDeleteView(DeleteView):
     model = Blog
@@ -23,4 +46,10 @@ class BlogDeleteView(DeleteView):
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
