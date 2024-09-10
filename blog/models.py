adf33916aa7e3b6b05f412424374a5416e919db8
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-10 18:41:21 +0300

    correct model blog slug

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ NULLABLE = {"null": True, "blank": True}
 class Blog(models.Model):
     """ Класс для модели Блог """
     title = models.CharField(max_length=100, verbose_name="Заголовок", help_text="Введите заголовок")
-    slug = models.CharField(max_length=100, verbose_name="Путь", **NULLABLE)
+    slug = models.CharField(max_length=100, verbose_name="Путь")
     post = models.TextField(verbose_name="Статья", help_text="Введите текс")
     image = models.ImageField(upload_to="blog/", verbose_name="Фото", **NULLABLE, help_text="Загрузите фото")
     created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
