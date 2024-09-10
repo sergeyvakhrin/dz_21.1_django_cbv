e897944c11f436b348524f9c236e7d3700c0b559
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-10 19:32:58 +0300

    chenge pk on slug

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('<slug>/', BlogDetailView.as_view(), name='blog_detail'),
     path('create/', BlogCreateView.as_view(), name="blog_create"),
-    path('<int:pk>/update/', BlogUpdateView.as_view(), name="blog_update"),
-    path('<int:pk>/delete/', BlogDeleteView.as_view(), name="blog_delete"),
+    path('<slug>/update/', BlogUpdateView.as_view(), name="blog_update"),
+    path('<slug>/delete/', BlogDeleteView.as_view(), name="blog_delete"),
 ]
